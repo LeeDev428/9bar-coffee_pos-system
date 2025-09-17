521b832ee3cb4be407784e3f09dfe25d839e41f8
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-09-17 22:13:02 +0800

    fixed logout.php

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+// Logout functionality
+session_start();
+
+// Destroy all session data
+session_unset();
+session_destroy();
+
+// Redirect to login page
+header('Location: login.php');
+exit();
+?>
